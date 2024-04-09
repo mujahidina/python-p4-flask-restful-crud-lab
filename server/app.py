0eae3d380f96a6938e7a1d6b8d65c0aa30de4144
mujahidina <mujahidabdi5070@gmail.com>
2024-04-09 15:38:22 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -46,6 +46,27 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        for attr in request.json:
+            setattr(plant, attr, request.json[attr])
+        db.session.add(plant)
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
+    
+
+    def delete(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        response = make_response('', 204)
+
+        return response
+
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
